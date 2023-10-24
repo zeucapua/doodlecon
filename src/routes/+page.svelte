7b6397405f7220723ab335e8d7d33c35ebf74419
Zeu Capua <zeucapua@gmail.com>
2023-10-23 18:58:09 -0700

    init styling, etc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Doodler from "./Doodler.svelte";
+</script>
+
+
+<div class="flex flex-col w-full h-full items-center text-white min-w-screen min-h-screen p-8 bg-violet-950">
+  <main class="flex flex-col w-full h-full max-w-3xl items-center">
+    <Doodler />
+  </main>
+</div>
