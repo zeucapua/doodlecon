d441eebb1e7cc5c12877ab6a60bbd163665ad58d
Zeu Capua <zeucapua@gmail.com>
2023-10-25 13:42:05 -0700

    zlib fix?

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,18 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import * as dotenv from "dotenv";
+dotenv.config();
+
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
